eac084a1fa49c591911fc7d3895496ffb938dcdb
Renan Henrique Delalibera <85879423+Renan-libera@users.noreply.github.com>
2023-09-05 19:40:21 -0300

    Update if.cpp

--- if.cpp
+++ if.cpp
@@ -6,10 +6,10 @@ main(){
 	printf("Digite um numero: ");
 	scanf("%d",&num);
 	
-	if(num<10)
+	if(num>10)
 	printf("\n \n O numero é maior que 10");
 		
-	if (num>10)
+	if (num<10)
 	printf("\n\n O numero é menor que 10");
 	
 	else
